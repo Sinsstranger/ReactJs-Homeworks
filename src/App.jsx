9c5d1480424cb86b65d94bc53d632584bb2b706b
sinstranger <sinstranger@yandex.ru>
2024-01-11 16:23:55 +0300

    Работа с состоянием компонентов

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import getFilms from '@helpers/GetFilms.js';
+import { Tabs, Tab } from 'react-bootstrap';
 import FilmsList from '@components/FilmsList.jsx';
 import '@css/App.scss';
 
 function App() {
 	const [films, setFilms] = useState(null);
 	const [isLoading, toogleLoading] = useState(true);
+	const [key, setKey] = useState('tab1');
 	useEffect(() => {
 		const mount = async () => {
 			const filmsData = await getFilms();
@@ -16,7 +18,27 @@ function App() {
 	}, []);
 	return (
 		<div className="films-list">
-			{isLoading ? 'Идет загрузка' : <FilmsList filmsList={films.results} />}
+			{isLoading ? (
+				'Идет загрузка'
+			) : (
+				<Tabs
+					id="controlled-tabs"
+					activeKey={key}
+					onSelect={(k) => setKey(k)}>
+					<Tab
+						eventKey="tab1"
+						title="Фильмы">
+						{/* Содержимое первой вкладки */}
+						<FilmsList filmsList={films.results} />
+					</Tab>
+					<Tab
+						eventKey="tab2"
+						title="Сериалы">
+						{/* Содержимое второй вкладки */}
+						<FilmsList filmsList={films.results} />
+					</Tab>
+				</Tabs>
+			)}
 		</div>
 	);
 }
